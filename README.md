390d8bcc42b9c9965e9055ad6385a55ba11a5ca1
yonge.li <729861494@qq.com>
2023-04-25 21:57:27 +0800

    auto generate markdown

---
+++ README.md
@@ -0,0 +1,90 @@
+# gogendoc
+
+文档生成工具
+
+## MarkDown生成说明
+
+### 示例
+
+```go
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/liyonge-cm/gogendoc"
+	"github.com/liyonge-cm/gogendoc/markdown"
+
+)
+
+type Common struct {
+	UserID   int    `json:"-" v:"required" comment:"渠道用户ID"`
+	UserName string `json:"UserName" v:"required" comment:"渠道用户名称"`
+}
+type ChannelUser struct {
+	UserID   int    `json:"UserID" v:"required" comment:"渠道用户ID"`
+	UserName string `json:"UserName" v:"required" comment:"渠道用户名称"`
+	Channel  string `json:"Channel" v:"required" comment:"渠道"`
+}
+type CreateUserInfo struct {
+	Common
+	UserAge     int         `json:"UserAge" v:"required" comment:"用户年龄"`
+	UserName    string      `json:"UserName" v:"required" comment:"用户名称"`
+	Type        string      `json:"Type" v:"required" comment:"用户类型"`
+	ChannelUser ChannelUser `json:"ChannelUser" comment:"来源渠道用户"`
+	ValidTime   int         `json:"ValidTime" v:"required" comment:"有效期"`
+}
+
+type CreateUserInfoResponse struct {
+	Code    int         `json:"RetCode"`
+	Message string      `json:"Message"`
+	Data    interface{} `json:"Data"`
+}
+
+func TestGenDoc(t *testing.T) {
+	// 实例化文档
+	doc := gogendoc.NewDocument(&gogendoc.Document{
+		Title:   "用户接口文档",
+		Author:  gogendoc.Author,
+		BaseUrl: "http://xxx",
+	})
+	doc.AddItem("创建用户信息", "CreateUserInfo", gogendoc.POST, &CreateUserInfo{}, &CreateUserInfoResponse{Data: &CreateUserInfo{}})
+	doc.GenerateFields()
+	list := doc.GetList()
+	b, err := json.Marshal(list)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(string(b))
+}
+
+func TestGenMarkDown(t *testing.T) {
+	// 实例化文档
+	doc := gogendoc.NewDocument(&gogendoc.Document{
+		Title:       "用户接口文档1111",
+		Version:     "1.0.0",
+		Author:      gogendoc.Author,
+		BaseUrl:     "http://xxx",
+		FileNameKey: "title", //  title、url
+	})
+	// 自定义字段名称
+	// doc.GetFieldName(func(field reflect.StructField) string { return field.Tag.Get("json") })
+
+	// 自定义字段必填
+	// doc.GetFieldRequired(func(field reflect.StructField) bool { return true })
+
+	// 自定义字段说明
+	// doc.GetFieldDescription(func(field reflect.StructField) string { return field.Tag.Get("desc") })
+
+	// 添加接口
+	doc.AddItem("创建用户信息", "/createUserInfo", gogendoc.POST, &CreateUserInfo{}, &CreateUserInfoResponse{Data: &CreateUserInfo{}})
+
+	// 生成字段
+	doc.GenerateFields()
+	// 实例化文档生成器
+	md := markdown.New(doc)
+	// 生成文档
+	md.Generate("./docs")
+}
+```
---
+++ markdown/markdown.go
@@ -0,0 +1,226 @@
+package markdown
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/liyonge-cm/gogendoc"
+)
+
+type SubTable struct {
+	Title  string
+	Fields []gogendoc.Field
+}
+
+type Markdown struct {
+	doc         *gogendoc.Document
+	id          int
+	subReqList  []SubTable
+	subRespList []SubTable
+}
+
+func New(doc *gogendoc.Document) *Markdown {
+	return &Markdown{doc: doc, subReqList: make([]SubTable, 0), subRespList: make([]SubTable, 0)}
+}
+
+func (m *Markdown) Generate(file string) {
+	err := deleteDir(file)
+	if err != nil {
+		panic(err)
+	}
+	err = createDir(file)
+	if err != nil {
+		panic(err)
+	}
+
+	readmeFile := path.Join(file, "README.md")
+	readmePage := m.RenderReadmePage(m.doc)
+	err = createFile(readmeFile, []byte(readmePage))
+	if err != nil {
+		panic(err)
+	}
+
+	list := m.doc.GetList()
+	for i, item := range list {
+
+		pageFile := path.Join(file, string(item.Url)+".md")
+		if m.doc.FileNameKey == "title" {
+			pageFile = path.Join(file, item.Title+".md")
+		}
+		page := m.RenderPage(i, item)
+
+		_ = os.Remove(pageFile)
+		err := createFile(pageFile, []byte(page))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func (m *Markdown) RenderReadmePage(v *gogendoc.Document) string {
+	ts := TplPage
+	ts = strings.Replace(ts, "{title}", v.Title, 1)
+	ts = strings.Replace(ts, "{version}", v.Version, 1)
+	ts = strings.Replace(ts, "{author}", v.Author, 1)
+	ts = strings.Replace(ts, "{baseUrl}", v.BaseUrl, 1)
+	return ts
+}
+
+func (m *Markdown) RenderPage(id int, v gogendoc.DocItem) string {
+	m.subReqList = make([]SubTable, 0)
+	m.subRespList = make([]SubTable, 0)
+	m.id = id
+	ts := TplBody
+	ts = strings.Replace(ts, "{id}", fmt.Sprintf("%v", id), 1)
+	ts = strings.Replace(ts, "{name}", v.Title, 1)
+	ts = strings.Replace(ts, "{method}", string(v.Method), 1)
+	ts = strings.Replace(ts, "{url}", string(v.Url), 1)
+	if len(v.ReqFields) > 0 {
+		reqTable := m.RenderReqTable("", v.ReqFields)
+		if len(m.subReqList) > 0 {
+			subTable := ""
+			for _, item := range m.subReqList {
+				tpl := m.RenderReqTable(item.Title, item.Fields)
+				subTable = fmt.Sprintf("%s%s", subTable, tpl)
+			}
+			reqTable = fmt.Sprintf("%s%s", reqTable, subTable)
+		}
+		ts = strings.Replace(ts, "{reqTable}", reqTable, 1)
+	} else {
+		ts = strings.Replace(ts, "{reqTable}", "", 1)
+	}
+	if len(v.RespFields) > 0 {
+		respTable := m.RenderRespTable("", v.RespFields)
+		if len(m.subRespList) > 0 {
+			subTable := ""
+			for _, item := range m.subRespList {
+				tpl := m.RenderRespTable(item.Title, item.Fields)
+				subTable = fmt.Sprintf("%s%s", subTable, tpl)
+			}
+			respTable = fmt.Sprintf("%s%s", respTable, subTable)
+		}
+		ts = strings.Replace(ts, "{respTable}", respTable, 1)
+	} else {
+		ts = strings.Replace(ts, "{respTable}", "", 1)
+	}
+	if v.ReqParam != nil {
+		reqParam, _ := json.MarshalIndent(v.ReqParam, "", "\t")
+		ts = strings.Replace(ts, "{reqParam}", fmt.Sprintf("```json\n %s \n```", string(reqParam)), 1)
+	} else {
+		ts = strings.Replace(ts, "{reqParam}", "", 1)
+	}
+	if v.RespParam != nil {
+		respParam, _ := json.MarshalIndent(v.RespParam, "", "\t")
+		ts = strings.Replace(ts, "{respParam}", fmt.Sprintf("```json\n %s \n```", string(respParam)), 1)
+	} else {
+		ts = strings.Replace(ts, "{respParam}", "", 1)
+	}
+	return ts
+
+}
+
+func (m *Markdown) RenderReqTable(title string, fields []gogendoc.Field) string {
+	ts := ""
+	if title != "" {
+		ts = fmt.Sprintf("\n<a id=\"%d.%s\"></a> \n##### %s \n %s ", m.id, title, title, TplReqTable)
+	} else {
+		ts = TplReqTable
+	}
+
+	params := ""
+	for _, v := range fields {
+		tpl := m.RenderReqParam(v)
+		params = fmt.Sprintf("%s%s", params, tpl)
+	}
+	ts = strings.Replace(ts, "{params}", params, 1)
+	return ts
+}
+
+func (m *Markdown) RenderReqParam(v gogendoc.Field) string {
+	ts := TplReqParam
+	required := "是"
+	if !v.Required {
+		required = "否"
+	}
+	ts = strings.Replace(ts, "{name}", v.Name, 1)
+	ts = strings.Replace(ts, "{type}", v.Type, 1)
+	ts = strings.Replace(ts, "{required}", required, 1)
+	if len(v.List) > 0 {
+		subTable := SubTable{
+			Title:  v.Name,
+			Fields: v.List,
+		}
+		m.subReqList = append(m.subReqList, subTable)
+		v.Description = fmt.Sprintf("%s [点我](#%d.%s)", v.Description, m.id, v.Name)
+	}
+	ts = strings.Replace(ts, "{description}", v.Description, 1)
+	return ts
+}
+
+func (m *Markdown) RenderRespTable(title string, fields []gogendoc.Field) string {
+	ts := ""
+	if title != "" {
+		ts = fmt.Sprintf("\n<a id=\"%d.%s\"></a> \n##### %s \n %s ", m.id, title, title, TplRespTable)
+	} else {
+		ts = TplRespTable
+	}
+	params := ""
+	for _, v := range fields {
+		tpl := m.RenderRespParam(v)
+		params = fmt.Sprintf("%s%s", params, tpl)
+	}
+	ts = strings.Replace(ts, "{params}", params, 1)
+	return ts
+}
+
+func (m *Markdown) RenderRespParam(v gogendoc.Field) string {
+	ts := TplRespParam
+	ts = strings.Replace(ts, "{name}", v.Name, 1)
+	ts = strings.Replace(ts, "{type}", v.Type, 1)
+	if len(v.List) > 0 {
+		subTable := SubTable{
+			Title:  v.Name,
+			Fields: v.List,
+		}
+		m.subRespList = append(m.subRespList, subTable)
+		v.Description = fmt.Sprintf("%s [点我](#%d.%s)", v.Description, m.id, v.Name)
+	}
+	ts = strings.Replace(ts, "{description}", v.Description, 1)
+	return ts
+}
+
+func createFile(fileName string, val []byte) error {
+	f, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = f.Write(val)
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path) //os.Stat获取文件信息
+	if err != nil {
+		return os.IsExist(err)
+	}
+	return true
+}
+
+func createDir(path string) error {
+	if !fileExists(path) {
+		err := os.Mkdir(path, os.ModePerm)
+		return err
+	}
+	return nil
+}
+
+func deleteDir(path string) error {
+	return os.RemoveAll(path)
+}
